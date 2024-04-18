6b268771ed63d603b7c03a1cfa756584b22947b4
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-18 14:41:46 -0500

    installations

--- 10.installations.sh
+++ 10.installations.sh
@@ -6,8 +6,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this script with root access."
+    exit 1
 else
     echo "You aresuper user."
 fi
-    
+
 dnf install mysql -y
+
+echo "is script proceeding?"
