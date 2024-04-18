7c6dded5d4743d343194b7d5bb665d3c47e179e3
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-18 17:54:23 -0500

    installpackages

--- 14.installpackages.sh
+++ 14.installpackages.sh
@@ -1,4 +1,17 @@
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+validate() {
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2....FAILURE"
+        exit 1   
+    else
+        echo "$2...SUCCESS"
+    fi    
+}
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this script with root access."
@@ -10,5 +23,12 @@ fi
 for i in $@
 do
     echo "Package to Install: $i"
-    dnf list installed $i
+    dnf list installed $i &>>LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed...SKIPPING"
+    else
+        echo "$i  not installed....Need to install"
+    fi    
+
 done
