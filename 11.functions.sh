bbb043c055b35d94f0f77c20ca9fd850af9866f8
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-18 16:44:42 -0500

    functions

---
+++ 11.functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+validate() {
+    echo "Exit status: $1"
+    echo "Whats the status: $2"
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "You aresuper user."
+fi
+
+dnf install mysql -y
+
+Validate $? "Installing MySQL"
+
+
+dnf install git -y
+Validate $? "Installing GIT"
