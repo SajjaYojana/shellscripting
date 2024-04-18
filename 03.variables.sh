74b1bec4d5d4268f8178c37238e182766997ac3c
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-18 13:15:12 -0500

    Variables3

---
+++ 03.variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+A=Lucky
+B=Honey
+echo "$A::Hi $B,How are you ?"
+echo "$B::Hi $A,I am fine, How about you?"
+echo "$A::I am fine, How about your work?"
+echo "$B::It is too slow and I want to upgrade to DevOps"
