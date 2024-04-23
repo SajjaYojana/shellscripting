f6f5f144f7b48a787b9ede34eb0f20b1da239d5e
Yojana Sajja <sajjayojana014@gmail.com>
2024-04-23 14:50:19 -0500

    Currentscript

--- 15-currentscript.sh
+++ 15-currentscript.sh
@@ -5,6 +5,6 @@ C="DEvops from Current Script"
 echo "Before calling the other script,course: $C"
 echo "Process ID of current shell script: $$"
 
-./16-otherscript.sh
+source ./16-otherscript.sh
 
 echo "After calling the other script,course: $C"
